df87de18f7c3c6ae652acc4f81a879a556a6dced
mohammad <mohahones@gmail.com>
2026-05-10 21:31:50 +0300

    add

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-let menuB = document.getElementById("mobile-menu");
-let navLinks = document.querySelector(".nav-ll");
-let navForm = document.querySelector(".nav-form");
+function menu() {
+  let menuB = document.getElementById("mobile-menu");
+  let navLinks = document.querySelector(".nav-ll");
+  let navForm = document.querySelector(".nav-form");
 
-menuB.addEventListener("click", (e) => {
+  menuB.addEventListener("click", (e) => {
     e.stopPropagation();
     navLinks.classList.toggle("active");
-});
+  });
 
-navForm.addEventListener("click", (e) => {
+  navForm.addEventListener("click", (e) => {
     e.stopPropagation();
-});
+  });
 
-document.body.addEventListener("click", () => {
+  document.body.addEventListener("click", () => {
     navLinks.classList.remove("active");
-});
+  });
+}
+
+menu();
+
+function darkLight() {
+  let light = document.querySelector(".light-mode");
+  let bodyy = document.body;
+  light.addEventListener("click", (e) => {
+    e.preventDefault();
+    bodyy.classList.toggle("active");
+    if (bodyy.classList.contains("active")) {
+      window.localStorage.setItem("darkMode", "enabled");
+    } else {
+      window.localStorage.setItem("darkMode", "disabled");
+    }
+  });
+}
+darkLight();
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("active");
+}
